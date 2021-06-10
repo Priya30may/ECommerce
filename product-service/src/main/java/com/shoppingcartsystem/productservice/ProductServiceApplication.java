d1ed0def2537a9d147b4dae4bdcf4c6f7fb39e64
Priya30may <priyadharshinisatheesh@gmail.com>
2021-06-10 10:58:32 +0530

    CRUD Product

---
+++ product-service/src/main/java/com/shoppingcartsystem/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingcartsystem.productservice;
+
+import org.springframework.boot.SpringApplication;
+
+
+
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
