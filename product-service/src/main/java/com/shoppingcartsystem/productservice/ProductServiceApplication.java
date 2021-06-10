ac108c4ebda4b8252c4c8917d1278d4ca9df567a
Priya30may <priyadharshinisatheesh@gmail.com>
2021-06-10 12:20:27 +0530

    Swagger

--- product-service/src/main/java/com/shoppingcartsystem/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/shoppingcartsystem/productservice/ProductServiceApplication.java
@@ -1,10 +1,24 @@
 package com.shoppingcartsystem.productservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-
-
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@SpringBootApplication
+@EnableSwagger2
 public class ProductServiceApplication {
+	
+	
+	@Bean public Docket swaggerConfiguration() { return new
+			Docket(DocumentationType.SWAGGER_2) .select()
+			.apis(RequestHandlerSelectors.basePackage("com.shoppingcartsystem.productservice"))
+			.build();
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
